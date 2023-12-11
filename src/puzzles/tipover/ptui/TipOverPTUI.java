ea26b21699f89072f83a5d04e3b3d5a3863f17bd
Alex DiMartino <awd8767@rit.edu>
2023-12-11 11:48:13 -0500

    backup

--- src/puzzles/tipover/ptui/TipOverPTUI.java
+++ src/puzzles/tipover/ptui/TipOverPTUI.java
@@ -4,8 +4,21 @@ import puzzles.common.Observer;
 import puzzles.tipover.model.TipOverModel;
 import puzzles.tipover.model.TipOverModel;
 
+import java.util.Scanner;
+
 public class TipOverPTUI implements Observer<TipOverModel, String> {
     private TipOverModel model;
+    private Scanner in;
+    private boolean gameOn;
+
+
+    public TipOverPTUI() {
+        model = new TipOverModel();
+        model.addObserver(this);
+        in = new Scanner(System.in);
+        gameOn = false;
+    }
+    
 
     @Override
     public void update(TipOverModel model, String message) {
@@ -15,5 +28,6 @@ public class TipOverPTUI implements Observer<TipOverModel, String> {
         if (args.length != 1) {
             System.out.println("Usage: java TipOverPTUI filename");
         }
+        TipOverPTUI ptui =new TipOverPTUI();
     }
 }
